56b154513004c147ed166c48f12b554c99187b20
marcusljx <marcusljx@gmail.com>
2016-01-22 18:08:36 +0800

    finished unique chunking into set.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <openssl/md5.h>
 #include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <cstring>
+#include <map>
+#include <bits/stl_set.h>
 
 using namespace std;
 
@@ -16,12 +16,41 @@ using namespace std;
 #define SLIDINGWINDOW_DIVISOR 3
 #define SLIDINGWINDOW_REMAINDER 2
 
+//======================================
+struct bin_entry {
+	string chunkID;
+	size_t chunkSize;
+};
+
+bool compareHexStrings(string A, string B) {
+	if(A.size() == B.size()) { // sizes same, compare characters
+		for(int i=0; i<A.size(); i++) {
+			if(A[i] < B[i]) {
+				return true;
+			} else if (A[i] > B[i]) {
+				return false;
+			} // else check next character
+		}
+	}
+	// if sizes doesn't match, order by size (clearly 0x100 > 0xff)
+	return (A.size() < B.size());
+}
+
+struct chunkIDcompare {
+	bool operator() (const bin_entry* A, const bin_entry* B) const {	// assuming all chunkID's size is same.
+		return compareHexStrings(A->chunkID, B->chunkID);
+	}
+};
+
+typedef set<bin_entry* , chunkIDcompare> Bin;
+
+//======================================
 void m_err(string error_message) {
 	perror(error_message.c_str());
 	exit(EXIT_FAILURE);
 }
 
-unsigned int fingerprint(unsigned char *arr_ptr) {	// simple hash using xor
+unsigned int fingerprint(unsigned char *arr_ptr) {	// simple fingerprint
 	unsigned long tally = 0;
 	int size = sizeof(arr_ptr)/ sizeof(unsigned char);
 	for(int i=1; i<size; i++) {
@@ -30,6 +59,8 @@ unsigned int fingerprint(unsigned char *arr_ptr) {	// simple hash using xor
 
 	return (unsigned int) (tally % FINGERPRINT_DIVISOR);	// result will fit definitely within (unsigned int)
 }
+
+
 
 int main(int argc, char* argv[]) {
 	// read file into mapped memory
@@ -48,42 +79,52 @@ int main(int argc, char* argv[]) {
 	unsigned char* chunk_begin = contents;
 	unsigned char* chunk_end;
 
+	Bin fileBin;
+	bin_entry* newEntry;
+
 	for(off_t i=0; i<(fileLength-WINDOW_SIZE); i++) {
 		memcpy(slidingWindow, (contents+i), WINDOW_SIZE);
 		
-//		cout << fingerprint(slidingWindow);
 		if((fingerprint(slidingWindow) % SLIDINGWINDOW_DIVISOR == SLIDINGWINDOW_REMAINDER) || (i + WINDOW_SIZE == fileLength - 1 ) ){
-//			cout << "(Y) \t";
 			chunk_end = contents + i + WINDOW_SIZE;
 
 			// FIND CHUNK ID
 			size_t chunkSize = chunk_end - chunk_begin;
-//			cout << "chunkSize = " << chunk_end - chunk_begin ;
-//			cout << " \t[" << (void*)chunk_begin << " --> " << (void*)chunk_end << "]";
 			unsigned char CHUNK[chunkSize];
 			unsigned char DIGEST[MD5_DIGEST_LENGTH];
-			unsigned char hash[MD5_DIGEST_LENGTH];
+			unsigned char ChunkID[MD5_DIGEST_LENGTH];
 			memcpy(CHUNK, chunk_begin, chunkSize);
 			MD5((unsigned char *) &CHUNK, MD5_DIGEST_LENGTH, (unsigned char *) &DIGEST);
 			for(int j=0; j<MD5_DIGEST_LENGTH; j++) {
-				sprintf((char *) &hash[j * 2], "%02x", (unsigned int)DIGEST[j]);
+				sprintf((char *) &ChunkID[j * 2], "%02x", (unsigned int)DIGEST[j]);
 			}
+			cout << "[" << chunkSize << "] \t" << CHUNK << " --> " << ChunkID << " \t(" << strtol(
+					(const char *) ChunkID, NULL, 16) << ")";
 
-			cout << "[" << chunkSize << "] \t" << CHUNK << " --> " << hash;
+			// create a new entry in the bin
+			newEntry = new bin_entry;
+			string temp(reinterpret_cast<const char*> (ChunkID), strlen((const char *) ChunkID));
+
+			newEntry->chunkID = temp;
+			newEntry->chunkSize = chunkSize;
+			fileBin.insert(newEntry);
+			cout << "fileBin size = " << fileBin.size() << endl;
 
 			// set chunk begin to next chunk
 			chunk_begin = chunk_end + 1;
 			i += WINDOW_SIZE;	// shift window to next chunk
 			cout << endl;
-		} 
-
-
-
-		// MD5(slidingWindow, WINDOW_SIZE, digest);
-		// cout << slidingWindow << endl;
-		// cout << digest << endl;
-		// cout << fingerprint(slidingWindow) << endl;
+		}
 	}
+
+	// Representative Chunk ID is smallest value
+	string repChunkID = (*fileBin.begin())->chunkID;
+	string lastID = (*fileBin.rbegin())->chunkID;
+
+	cout << "Total number of Chunks = " << fileBin.size() << endl;
+
+	cout << "Representative Chunk ID: \t" << repChunkID << endl;
+	cout << "Last Chunk ID: \t" << lastID << endl;
 
 	munmap(contents, fileLength);
 	return 0;
